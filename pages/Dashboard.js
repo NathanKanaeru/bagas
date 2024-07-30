975f7aa71e02faf1b5f08db6d467e3fafbdc6798
Nathan's here <94665436+NathanKanaeru@users.noreply.github.com>
2024-07-30 23:15:04 +0700

    Create Dashboard.js

---
+++ pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DashboardContent from '../components/DashboardContent';
+
+const Dashboard = () => (
+    <div>
+        <DashboardContent />
+    </div>
+);
+
+export default Dashboard;
